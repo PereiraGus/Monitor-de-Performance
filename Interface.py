9daae5ee2e3650287f450ccf8e3bd6c3dd252b13
gba-exe <gabriel.baraujo@sptech.school>
2023-08-28 11:24:21 -0300

    feat(): banco de dados adicionado e variaveis funcionando

--- Interface.py
+++ Interface.py
@@ -2,11 +2,21 @@ from tkinter import *
 from tkinter import ttk
 from time import sleep as s
 import psutil as ps
+import mysql.connector
+
+tDados = mysql.connector.connect(
+    host="localhost",
+    user="gba",
+    password="2307",
+    database="monitorDeDados"
+)
+
+mycursor = tDados.cursor()
 
 root = Tk()
 root.title("Monitor de Performance")
 root.resizable(False,False)
-root.iconbitmap("./assets/favicon.ico")
+# root.iconbitmap("./assets/favicon.ico")
 
 # Informações do usuário ===============================================
 containerInfos = LabelFrame(root, text="Informações do sistema operacional")
@@ -57,6 +67,21 @@ RAMTot = StringVar()
 RAMAtual = StringVar()
 RAMPercent = StringVar()
 
+dadoCPUFisc = ps.cpu_count(False)
+dadoCPULogc = ps.cpu_count(True)
+dadoCPUFreq = ps.cpu_freq(False).current
+dadoCPUPercent = round(100-ps.cpu_times_percent(interval=1)[2],1)
+
+dadoHDNumParcs = len(ps.disk_partitions(True))
+dadoHDTotal = round((ps.disk_usage("/").total)*10**-9,2)
+dadoHDAtual = round((ps.disk_usage("/").used)*10**-9,2) 
+dadoHDPercent = ps.disk_usage("/").percent
+
+dadoRAMTot = round((ps.virtual_memory().total)*10**-9,2)
+dadoRAMAtual = round((ps.virtual_memory().used)*10**-9,2)
+dadoRAMPercent = ps.virtual_memory().percent
+
+
 dados = ((CPUFisc,CPULogc,CPUFreq,CPUPercent),(HDNumParcs,HDTotal,HDAtual,HDPercent),(RAMTot,RAMAtual,RAMPercent))
 
 indiceGrid = 0
@@ -75,18 +100,26 @@ for coluna in colunas:
     indiceGrid += 1
 
 while(True):
-    dados[0][0].set(str(ps.cpu_count(False)))
-    dados[0][1].set(str(ps.cpu_count(True)))
-    dados[0][2].set(str(ps.cpu_freq(False).current)+"MHz")
-    dados[0][3].set(str(round(100-ps.cpu_times_percent(interval=1)[2],1))+"%")
+    dados[0][0].set(str(dadoCPUFisc))
+    dados[0][1].set(str(dadoCPULogc))
+    dados[0][2].set(str(dadoCPUFreq)+"MHz")
+    dados[0][3].set(str(dadoCPUPercent)+"%")
 
-    dados[1][0].set(str(len(ps.disk_partitions(True))))
-    dados[1][1].set(str(round((ps.disk_usage("/").used)*10**-9,2))+"GB")
-    dados[1][2].set(str(round((ps.disk_usage("/").total)*10**-9,2))+"GB")
-    dados[1][3].set(str(ps.disk_usage("/").percent)+"%")
-
-    dados[2][0].set(str(round((ps.virtual_memory().total)*10**-9,2))+"GB")
-    dados[2][1].set(str(round((ps.virtual_memory().used)*10**-9,2))+"GB")
-    dados[2][2].set(str(ps.virtual_memory().percent)+"%")
+    dados[1][0].set(str(dadoHDNumParcs))
+    dados[1][1].set(str(dadoHDTotal)+"GB")
+    dados[1][2].set(str(dadoHDAtual)+"GB")
+    dados[1][3].set(str(dadoHDPercent)+"%")
+    
+    dados[2][0].set(str(dadoRAMTot)+"GB")
+    dados[2][1].set(str(dadoRAMAtual)+"GB")
+    dados[2][2].set(str(dadoRAMPercent)+"%")
     root.update()
     s(int(tempoAtualizacao.get()))
+
+    sql = "INSERT INTO dados VALUES (null, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, now())"
+    val = (dadoCPUFisc,dadoCPULogc,dadoCPUFreq,dadoCPUPercent,dadoHDNumParcs,dadoHDTotal,dadoHDAtual ,dadoHDPercent,dadoRAMTot,dadoRAMAtual,dadoRAMPercent )
+    mycursor.execute(sql, val)
+
+    tDados.commit()
+
+    print(mycursor.rowcount, "record inserted.")
