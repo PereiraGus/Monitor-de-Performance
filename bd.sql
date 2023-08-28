f9e8691f91113eff1d377b5dd5b158c66d58b203
PereiraGus <gustavo.oito@hotmail.com>
2023-08-28 13:17:07 -0300

    Fixes do banco

--- bd.sql
+++ bd.sql
@@ -1,17 +1,24 @@
+drop database if exists monitorDeDados;
 create database monitorDeDados;
 use monitorDeDados;
 
+create user "acessoProducao" identified by "urubu100";
+grant INSERT,SELECT on monitorDeDados.* to "acessoProducao";
+flush privileges;
+
 create table dados ( idDado int auto_increment not null primary key,
-                     procFisico int
-                     procLogico int
-                     freqCPU double
-                     percUso double
-                     particoes int
-                     totalDisco double
-                     usoAtualDisco double
-                     percUsoDisco double
-                     ramTotal double
-                     usoAtualRam double
-                     percUsoRam double
+                     procFisico int,
+                     procLogico int,
+                     freqCPU double,
+                     percUso double,
+                     particoes int,
+                     totalDisco double,
+                     usoAtualDisco double,
+                     percUsoDisco double,
+                     ramTotal double,
+                     usoAtualRam double,
+                     percUsoRam double,
                      dataHora datetime
                    );
+                   
+select * from dados;
